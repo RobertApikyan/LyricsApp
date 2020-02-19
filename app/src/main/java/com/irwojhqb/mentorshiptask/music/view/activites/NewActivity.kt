60ebc016cfbe6d8b9ffa40c3b4ea28a16b404e68
zveda21 <zveda.hayrapetyan.97@mail.ru>
2020-02-19 18:31:02 +0400

    new activity

---
+++ app/src/main/java/com/irwojhqb/mentorshiptask/music/view/activites/NewActivity.kt
@@ -0,0 +1,13 @@
+package com.irwojhqb.mentorshiptask.music.view.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.irwojhqb.mentorshiptask.R
+
+class NewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+    }
+}
