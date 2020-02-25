9e33f26ce8474ab0f41175c0cb45c606aea25195
robert <robert.apikyan@volo.global>
2020-02-25 10:44:02 +0400

    asdasd

--- app/src/main/java/com/irwojhqb/mentorshiptask/music/viewmodel/MyViewModel.kt
+++ app/src/main/java/com/irwojhqb/mentorshiptask/music/viewmodel/MyViewModel.kt
@@ -21,21 +21,22 @@ class MyViewModel(application: Application) : AndroidViewModel(application) {
 
     fun getMusicData() {
         MyRepo.getMusic().enqueue(object : Callback<Response> {
-                override fun onFailure(call: Call<Response>, t: Throwable) {
-                    Log.d("MyViewModel__ERROR", t.message!!)
+            override fun onFailure(call: Call<Response>, t: Throwable) {
+                Log.d("MyViewModel__ERROR", t.message!!)
+            }
+
+            override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
+                if (response.isSuccessful) {
+                    // todo avoid force unwraps and force casts
+                    modelmutableLiveData.value =
+                        response.body()!!.response!!.songs as MutableList<SongsItem>?
+                    Log.d(
+                        "MyViewModel",
+                        "Hello-- = ${modelmutableLiveData.value!![0].title}"
+                    )
                 }
-                override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
-                    if (response.isSuccessful) {
-                        // todo avoid force unwraps and force casts
-                        modelmutableLiveData.value =
-                            response.body()!!.response!!.songs as MutableList<SongsItem>?
-                        Log.d(
-                            "MyViewModel",
-                            "Hello-- = ${modelmutableLiveData.value!![0].title}"
-                        )
-                    }
-                }
-            })
+            }
+        })
     }
 
 }
